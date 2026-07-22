eca16fe461b6865730ef2a21cc9a2a959be9c6ab
Mochammad Azka Basria <pindandss2@gmail.com>
2026-07-22 14:23:13 +0700

    feat: implement subjects GET route with search, filter, and pagination

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,10 +1,65 @@
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
 import { Router, type Request, type Response } from "express";
-
+import { departments, subjects } from "../db/schema/app.js";
+import { db } from "../db.js";
+import { match } from "node:assert";
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   try {
-    // TODO: Logika database akan ditambahkan nanti
+    const { search, department, page = 1, limit = 10 } = req.query;
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filterConditions = [];
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+
+    if (department) {
+      filterConditions.push(ilike(departments.name, `%${department}%`));
+    }
+
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+
+    const totalCount = countResult[0]?.count ?? 0;
+
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: {
+          ...getTableColumns(departments),
+        },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
   } catch (error) {
     console.error("GET /subjects ERROR:", error);
     res.status(500).json({ error: "Internal Server Error" });
